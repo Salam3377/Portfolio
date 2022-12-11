d31c4bd45d9460fd8725a66393af5787f8eb204f
Salam337 <salamadash@gmail.com>
2022-12-10 19:41:02 -0500

    added button functions and some images

--- portfolio/src/components/Home.js
+++ portfolio/src/components/Home.js
@@ -1,15 +1,31 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import '../components/home.css'
+import pongMain from '../images/pong-main-pic.PNG'
+import coffeeShop from '../images/coffee-shop.PNG'
+import emailjs from '@emailjs/browser';
 
 
 const Home = () => {
+    const form = useRef();
+
+    const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_9vv00dm', 'template_xh8bbrf', form.current, 'PMOhg-FNFQLZkVJC4')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+    };
+
     const [showProject1, setShowProject1] = useState(false)
     const [showProject2, setShowProject2] = useState(false) 
     const [showProject3, setShowProject3] = useState(false)
      
-    const transition1 = showProject1 ? {width: '23vw', zIndex: '2'} : {width: '0vw'} ;
-    const transition2 = showProject2 ? {width: '23vw', zIndex: '2'} : {width: '0vw'} ;
-    const transition3 = showProject3 ? {width: '23vw', zIndex: '2'} : {width: '0vw'} ;
+    const transition1 = showProject1 ? {width: '27vw', zIndex: '2'} : {width: '0vw'} ;
+    const transition2 = showProject2 ? {width: '27vw', zIndex: '2'} : {width: '0vw'} ;
+    const transition3 = showProject3 ? {width: '27vw', zIndex: '2'} : {width: '0vw'} ;
 
     const show1 = () => {
         setShowProject1(!showProject1)
@@ -52,7 +68,8 @@ const Home = () => {
 
             <div id="middle-body-div">
                 <div id="side-div">
-
+                    <a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/salam-abdusalamov05/" id="linkedin" class="fa fa-linkedin-square"><i></i></a>
+                   <a target="_blank" rel="noopener noreferrer" href="https://github.com/Salam3377" id="github" class="fa fa-github-square" ><i></i></a>
                 </div>
 
                 <div id="project-div">
@@ -64,19 +81,25 @@ const Home = () => {
                             <button onClick={() => show3()} class="project-buttons">Lux-House</button>
                         </div>
                     </div>
-                    <div style={transition1} id="project1-description-div">
 
-                    </div>
-                    <div style={transition2} id="project2-description-div">
+                    <img style={transition1} id="project1-description-img" alt="" src={pongMain}/>
 
-                    </div>
-                    <div style={transition3} id="project3-description-div">
+                    <img style={transition2} id="project2-description-img" alt="" src={coffeeShop} />
 
-                    </div>
+                    <img style={transition3} id="project3-description-img" alt="" />
                 </div>
                 
                 <div id="contact-me">
-
+                    <form id="contact-form" ref={form} onSubmit={sendEmail}>
+                        <h1 id="contact-me-h1">Contact Me</h1>
+                        <label class="label">Name</label>
+                        <input required placeholder="Your name" class="input" type="text" name="user_name" />
+                        <label  class="label">Email</label>
+                        <input required placeholder="Your email" class="input" type="email" name="user_email" />
+                        <label  class="label">Message</label>
+                        <textarea required placeholder="Your Message..." id="text-area" name="message" />
+                        <input id="button-send" type="submit" value="Send" />
+                    </form>
                 </div>
             </div>
             
