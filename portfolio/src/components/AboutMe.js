f0c4b57705709e613f5ecafd64690fbdb14d16cd
Salam337 <salamadash@gmail.com>
2024-03-16 21:14:32 -0400

    resumeUpdate

--- portfolio/src/components/AboutMe.js
+++ portfolio/src/components/AboutMe.js
@@ -23,7 +23,7 @@ function AboutMe() {
 
         <div id='links-div'>
         <a id='links' target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/salam-abdusalamov05/" className="fa fa-linkedin-square"> </a>
-        <a  id="resume-link" target="_blank" rel="noopener noreferrer"  href="https://docs.google.com/document/d/18J387r9X9s5Wh9QXe5aYxk6RaRDARTvFGfqLFC_E0jk/edit?usp=sharing" className="fa">&#xf15c;</a>
+        <a  id="resume-link" target="_blank" rel="noopener noreferrer"  href="https://drive.google.com/file/d/1psdY34W9kxkJB3H_f7ph6PwQJ-TLOp--/view?usp=sharing" className="fa">&#xf15c;</a>
         <a id='links' target="_blank" rel="noopener noreferrer" href="https://github.com/Salam3377" className="fa fa-github-square"> </a>
       </div>
     </div>
